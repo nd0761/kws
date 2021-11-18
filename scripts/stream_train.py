c3359fc8186914d7ca175287f727921aa70b443b
nd0761 <deni,nat.and@gmail.com>
2021-11-18 16:21:26 +0300

    Add stream class

--- scripts/stream_train.py
+++ scripts/stream_train.py
@@ -4,70 +4,24 @@ import torch.nn.functional as F
 from scripts.metrics import count_FA_FR, get_au_fa_fr
 
 
-def train_epoch(model, opt, loader, log_melspec, device):
-    model.train()
-    for i, (batch, labels) in tqdm(enumerate(loader), total=len(loader)):
-        batch, labels = batch.to(device), labels.to(device)
-        batch = log_melspec(batch)
+class StreamingKws:
+    def __init__(self, max_window_length=40, streaming_step_size=3):
+        self.max_window_length = max_window_length
+        self.streaming_step_size = streaming_step_size
 
-        opt.zero_grad()
-
-        # run model # with autocast():
-        logits = model(batch)
-        # we need probabilities so we use softmax & CE separately
-        probs = F.softmax(logits, dim=-1)
-        loss = F.cross_entropy(logits, labels)
-
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 5)
-
-        opt.step()
-
-        # logging
-        argmax_probs = torch.argmax(probs, dim=-1)
-        FA, FR = count_FA_FR(argmax_probs, labels)
-        acc = torch.sum(argmax_probs == labels) / torch.numel(argmax_probs)
-
-    return acc
-
-
-@torch.no_grad()
-def validation(model, loader, log_melspec, device, max_window_length=40, streaming_step_size=3):
-    model.eval()
-
-    val_losses, accs, FAs, FRs = [], [], [], []
-    all_probs, all_labels = [], []
-    hidden = None
-    for i, (batch, labels) in tqdm(enumerate(loader)):
-        batch, labels = batch.to(device), labels.to(device)
-        batch = log_melspec(batch)
-
-        last_id = batch.shape[1] - max_window_length + 1
-
+    @torch.no_grad()
+    def validation(self, model, x):
+        model.eval()
         probs = []
-        output = None
-        for ids in range(0, last_id, streaming_step_size):
-            output, hidden = model(batch[:, :, ids:ids + max_window_length], hidden, True)
+        hidden = None
+
+        for ids in range(0, x.shape[1], self.streaming_step_size):
+            if ids + self.max_window_length > x.shape[1]:
+                break
+            output, hidden = model(x[:, ids:ids + self.max_window_length], hidden, True)
 
             # we need probabilities so we use softmax & CE separately
             probs.append(F.softmax(output, dim=-1)[..., 1].unsqueeze(-1))
 
         probs = torch.cat(probs, dim=-1)
-        loss = F.cross_entropy(output, labels)
-
-        # logging
-        argmax_probs = torch.argmax(probs, dim=-1)
-        all_probs.extend(probs.cpu())
-        all_labels.append(labels.cpu())
-        val_losses.append(loss.item())
-        accs.append(
-            torch.sum(argmax_probs == labels).item() /  # ???
-            torch.numel(argmax_probs)
-        )
-        FA, FR = count_FA_FR(argmax_probs, labels)
-        FAs.append(FA)
-        FRs.append(FR)
-
-    # area under FA/FR curve for whole loader
-    au_fa_fr = get_au_fa_fr(torch.cat(all_probs, dim=0).cpu(), all_labels)
-    return au_fa_fr
+        return probs
